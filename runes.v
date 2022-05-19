1722eb26d1b07ba1bd7011a27407a7697b44d5c7
Kazuhiro Haraguchi <arx.solid@gmail.com>
2022-05-19 16:48:57 +0900

    add len_utf8

--- runes.v
+++ runes.v
@@ -2,11 +2,18 @@
 fn main() {
 	// string is an array of bytes(u8)
 	s := 'hello 世界'
-	println(s)
-	println(s.bytes())
-	println('s length $s.len\n')
+
+	mut msg := []string{}
+	msg << s
+	msg << 's.bytes(): $s.bytes()'
+	msg << 's.len: $s.len'
+	msg << 's.len_utf8: $s.len_utf8()'
+	msg << '\n'
+
 	// A rune represents a single Unicode character. Rune is an alias for `u32`
 	r := s.runes()
-	println(r)
-	println('r length $r.len')
+	msg << 'r := s.runes()'
+	msg << '$r'
+	msg << 'r.len: $r.len'
+	println(msg.join_lines())
 }
