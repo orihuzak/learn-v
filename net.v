ee3c61d7cd4a9a8560435360b2a25ab6e62ed284
Kazuhiro Haraguchi <arx.solid@gmail.com>
2022-07-25 02:38:03 +0900

    add resolve_ipaddrs

--- net.v
+++ net.v
@@ -1,7 +1,14 @@
-import net.http
+import net
 
 fn main() {
 	url := ""
-	res := http.get(url) or { panic("Failed http get $url") }
+	res := net.http.get(url) or { panic("Failed http get $url") }
 	println(res.text)
+
+	mut adrr := net.resolve_ipaddrs('google.com', .ip, .tcp) ?
+	println(adrr)
+
+	adrr = net.resolve_ipaddrs('github.com', .ip, .tcp) ?
+	println(adrr)
 }
+
